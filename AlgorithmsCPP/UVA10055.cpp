b2ea219a9706cb6e3366ffba7baa1af407f960b1
Minh Khue <lmk19922000@gmail.com>
2014-12-05 02:28:28 +0800

    UVA 10055

---
+++ AlgorithmsCPP/UVA10055.cpp
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "iostream"
+
+using namespace std;
+
+int main(){
+	long long a, b;
+
+	while (scanf("%lld %lld", &a, &b) != EOF){
+		if (a > b){
+			printf("%lld\n", a - b);
+		}
+		else{
+			printf("%lld\n", b-a);
+		}
+	}
+	return 0;
+}
